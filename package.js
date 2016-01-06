f3ceac67585fc3caf643c81940dba3fb78e8e843
David M. Pankros <dave@sqygl.com>
2016-01-06 11:28:53 -0500

    Updated version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'dpankros:timer',
-  version: '1.0',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'Easy to use timers for meteor',
   // URL to the Git repository containing the source code for this package.
